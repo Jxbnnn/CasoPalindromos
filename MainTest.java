ac225369e34c56247ee388487a992cb9c7270a2c
Alonso Lagos <137555754+alonshuu@users.noreply.github.com>
2025-04-23 21:52:39 -0400

    Archivo de test

---
+++ MainTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+class MainTest {
+
+    @Test
+    void testPalabraMinuscula () {
+        assertTrue(Main.esPalindromo("reconocer"));
+    }
+    @Test
+    void testPalabraMayuscula () {
+        assertTrue(Main.esPalindromo("Reconocer"));
+    }
+    @Test
+    void testFrase () {
+        assertTrue(Main.esPalindromo("anita lava la tina"));
+    }
+
+}
