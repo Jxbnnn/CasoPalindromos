177db6696c34c26202d4db6144b7a46307436758
benjamin <b.urrea02@ufromail.cl>
2025-04-23 20:59:30 -0400

    Actualizaciòn

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
 
 public class Main {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args){
+        System.out.println(esPalindromo("aca"));
     }
+
 
     public static boolean esPalindromo(String palabra){
         if (palabra.isEmpty()){
             return false;
         }
 
-        String lowercase = palabra.replaceAll("\\.s+","").toLowerCase();
+        String lowercase = palabra.replaceAll("\\s+","").toLowerCase();
 
         if (lowercase.isEmpty()){
             return false;
         }
-    }
-}
 
-//aaaa
+
+
+
+        char[] caracteres = lowercase.toCharArray();
+        char[] invertido = new char[caracteres.length];
+        for (int i = 0; i < caracteres.length; i++) {
+            invertido[i] = caracteres[caracteres.length - 1 - i];
+        }
